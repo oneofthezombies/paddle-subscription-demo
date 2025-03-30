7354e35b300d6efe6e23c11b338ecfca32292de0
oneofthezombies <hunhoekim@gmail.com>
2025-03-30 05:14:33 +0000

    chore: add timestamps to users

--- packages/web/db/schema.ts
+++ packages/web/db/schema.ts
@@ -1,8 +1,15 @@
-import { integer, pgTable, text } from "drizzle-orm/pg-core";
+import { integer, pgTable, text, timestamp } from "drizzle-orm/pg-core";
 
 export const usersTable = pgTable("users", {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
   emailEncrypted: text("email_encrypted").notNull(),
   emailHash: text("email_hash").notNull().unique(),
   passwordHash: text("password_hash").notNull(),
+  createdAt: timestamp("created_at", { withTimezone: true })
+    .notNull()
+    .defaultNow(),
+  updatedAt: timestamp("updated_at", { withTimezone: true })
+    .notNull()
+    .defaultNow(),
+  deletedAt: timestamp("deleted_at", { withTimezone: true }),
 });
